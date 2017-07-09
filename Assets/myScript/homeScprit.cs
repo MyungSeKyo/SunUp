bd7fb4759939190e8914f25495803e5cc52a3b61
MyungSeKyo <myungsekyo@gmail.com>
2017-07-09 16:00:59 +0900

    first commit

---
+++ Assets/myScript/homeScprit.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class homeScprit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void homeButtonClicked(){
+
+	}
+	public void quitButtonClicked(){
+
+	}
+	public void menuButtonClicked(){
+
+	}
+	public void rankButtonClicked(){
+
+	}
+	public void infoButtonClicked(){
+
+	}
+}
